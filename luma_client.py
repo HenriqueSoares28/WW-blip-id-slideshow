7ae29a4518c596fd969e431d9e08334e77e03fdd
Henrique França Carvalho Soares <henrique.soares.2811@gmail.com>
2025-07-16 18:26:35 -0300

    Atualiza a função de carregamento de vídeos para limitar a 200 gerações e remove o parâmetro de cache na requisição do JSON

--- luma_client.py
+++ luma_client.py
@@ -7,7 +7,7 @@ load_dotenv()
 client = LumaAI(auth_token=os.environ["LUMAAI_API_KEY"])
 
 def get_video_links():
-    generations = client.generations.list()
+    generations = client.generations.list(limit=200)
     return [
         gen.assets.video for gen in generations.generations
         if gen.assets and gen.assets.video
